5b5408692197dc50bc3fa4028cabd67cbb264949
Allister <ageebernal@aol.com>
2019-01-22 13:54:24 -0800

    second commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
+require('dotenv').load();
 const express = require('express');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath))
 
-app.get('/', (req, res) => {
-  res.send('Hello everyone');
-});
+
 
 app.listen(port, () => {
   console.log(`The server is on port ${port}`);
