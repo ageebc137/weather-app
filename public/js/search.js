ed4683ffe2b40ddd759813f716c245de7c98d424
Allister <ageebernal@aol.com>
2019-01-23 11:03:25 -0800

    Added Dark Sky API on backend

--- public/js/search.js
+++ public/js/search.js
@@ -1,1 +1,38 @@
-console.log('hello');
+const button = document.querySelector('#location');
+const temp = document.querySelector('#temperature');
+const currentLoc = {};
+let locationMod = {
+  latitude: '',
+  longitude: '',
+  query: ''
+}
+
+window.onload = navigator.geolocation.getCurrentPosition(function(position) {
+  locationMod.latitude = position.coords.latitude;
+  locationMod.longitude = position.coords.longitude;
+  locationMod.query = null;
+    getWeather(locationMod);
+});
+
+function buttonPress(e) {
+  e.preventDefault();
+  locationMod.latitude = null,
+  locationMod.longitude = null,
+  locationMod.query = encodeURIComponent(this.querySelector('input').value.trim());
+  console.log(locationMod.query);
+  getWeather(locationMod);
+}
+
+function getWeather(locationMod) {
+
+  axios.post('//localhost:4000/search', locationMod).then((res) => {
+    console.log(res);
+    temp.innerHTML = res.data.currently.temperature;
+  }).catch(err => {
+    if (err) {
+      return err;
+    }
+  });
+}
+
+button.addEventListener('submit', buttonPress);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
 require('dotenv').load();
+
+const axios = require('axios');
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const {getCoords} = require('../public/js/libs/location');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.static(publicPath))
+app.use(bodyParser.json());
+
+app.use(express.static(publicPath));
+
+app.get(('/'), (req, res) => {
+  res.render('index');
+})
+
+app.post(('/search'), (req, res) => {
+  console.log(req.body.latitude, req.body.longitude, req.body.query);
+  if (req.body.query) {
+    getCoords(req.body.query).then((response) => {
+      var latitude = response.data.results[0].locations[0].latLng.lat;
+      var longitude = response.data.results[0].locations[0].latLng.lng;
+      axios.get(`${process.env.DS_APIKEY}${latitude},${longitude}`).then(dsweather => {
+        res.send(dsweather.data);
+      }).catch(err => {
+        if(err) {
+          console.log(err)
+        }
+      })
+    });
+  }else{
+    axios.get(`${process.env.DS_APIKEY}${req.body.latitude}, ${req.body.longitude}`).then(dsweather => {
+      res.send(dsweather.data);
+    }).catch(err => {
+      if(err) {
+        console.log(err)
+      }
+    });
+  }
 
 
+
+});
 
 app.listen(port, () => {
   console.log(`The server is on port ${port}`);
