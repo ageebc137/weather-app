1a596f2190c5b06b000323437a7113d4628aa91c
Allister <ageebernal@aol.com>
2019-01-23 22:25:25 -0800

    test#5

--- public/js/libs/location.js
+++ public/js/libs/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 var getCoords = (query) => {
   console.log(`${process.env.MQ_APIKEY}${query}`);
-  return axios.get(`${process.env.MQ_APIKEY}${query}`);
+  return axios.get(`https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.MQ_APIKEY}&location=${query}`);
 }
 
 module.exports = {getCoords};
